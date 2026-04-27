fab3506dfb18a0e045e5b15a5c2b1c6572a1c4a1
Sneha Patil <snehapatil5581@gmail.com>
2026-04-27 17:18:32 +0530

    File

--- Experiments/Project 3 Express Backend/routes/contact.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.post("/", (req, res) => {
-    console.log(req.body);
-    res.send("Message received! Thank you for contacting us");
-});
-
-module.exports = router;
